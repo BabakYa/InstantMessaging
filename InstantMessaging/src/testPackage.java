55d440dbdb859fd526dad94edba0f54ea5412d6e
BabakYa <yadranjiaghdamb15@students.ecu.edu>
2016-05-07 22:42:10 -0400

    Added files via upload

---
+++ InstantMessaging/src/testPackage.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import java.net.Socket;
+
+import org.junit.Test;
+
+import Client.ChatClient;
+import Client.ChatClientThread;
+
+public class testPackage {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+		//test1();
+	}
+	
+	private void test1(){
+		//ChatClientThread cct = new ChatClientThread(ChatClient, new Socket("Abbas", 55555));
+		//ChatClient.connect("Abbas", 55555);
+		//ChatClient.checkConnect("Abbas", 55555);
+		
+	}
+
+}
